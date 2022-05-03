a1f823167a93c5ef04f67edd84e9745d125e9954
Тулегенов Самат Булатович <tulegenov.s_qschool@qsoft.ru>
2022-05-04 04:34:53 +0500

    Конец

---
+++ src/components/TodoList.vue
@@ -0,0 +1,33 @@
+/* eslint-disable */
+
+<template>
+  <div>
+    <ul>
+      <TodoItem v-for="(todo, index) in todos" v-bind:todo="todo" v-bind:index="index"  v-on:remove-todo="removeTodo"/>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem.vue';
+
+export default {
+  props: ['todos'],
+  components: {
+    TodoItem,
+  },
+  methods: {
+    removeTodo(id) {
+      this.$emit('remove-todo', id);
+    }
+  },
+};
+</script>
+
+<style scoped>
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Homepage</h2>
+    <router-link to="/todos">Todos</router-link>
+    <hr>
+
+    <p>Lorem</p>
+  </div>
+</template>
